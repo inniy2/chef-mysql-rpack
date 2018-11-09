a54f4f95400acaf400d600cb9b248b20eafe89e0
inniy2 <inniy2@gmail.com>
2018-11-09 16:42:53 +0900

    Init

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,112 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+
+#groupadd -g55000 mysql
+group 'mysql' do
+  gid 55000
+  action :create
+end
+
+# useradd -u 55000 -g 55000 -c 'mysql-user' -m -d '/usr/local/mysql' -s '/bin/bash' -p '*' mysql
+# chmod 755 /usr/local/mysql
+user 'mysql' do
+  comment 'mysql-user'
+  uid 55000
+  gid 'mysql'
+  home '/usr/local/mysql'
+  shell '/bin/bash'
+  password '*'
+end
+
+## download mysql repository
+remote_file '/usr/local/src/mysql57-community-release-el7-11.noarch.rpm' do
+    source 'https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm'
+    action :create
+end
+
+# sudo yum localinstall mysql57-community-release-el7-11.noarch.rpm
+yum_package 'mysql57-community-release-el7-11.noarch.rpm' do
+    source '/usr/local/src/mysql57-community-release-el7-11.noarch.rpm'
+    action :install
+end
+
+# yum install mysql-community-server
+# yum install http://repo.mysql.com/yum/mysql-5.7-community/el/7/x86_64/mysql-community-server-5.7.17-1.el7.x86_64.rpm
+# yum install mysql-community-server-5.7.17-1.el7.x86_64 (OK)
+yum_package 'mysql-community-server' do
+    version '5.7.17-1.el7'
+    action :install
+end
+
+# Create MySQL Directory
+# cd / ; mkdir -pvm 755 /MYSQL ; cd /MYSQL
+# mkdir -pvm 755 cloud101 ; cd cloud101;
+# mkdir -pvm 755 binlog data env innodb innodb-log log var work certs;
+# mkdir -pvm 777 /MYSQL/cloud101/work/tmp
+# cd ../ ; chown -R mysql:mysql cloud101 ;
+
+#directory '/MYSQL' do
+#  owner 'mysql'
+#  group 'mysql'
+#  mode '0755'
+#  action :create
+#end
+
+directory '/MYSQL/cloud101' do
+  recursive true
+  owner 'mysql'
+  group 'mysql'
+  mode '0755'
+  action :create
+end
+
+%w[ binlog data env innodb innodb-log log var work certs ].each do | path | 
+    directory "/MYSQL/cloud101/#{path}" do
+      owner 'mysql'
+      group 'mysql'
+      mode '0755'
+      action :create
+    end
+end
+
+directory '/MYSQL/cloud101/work/tmp' do
+  owner 'mysql'
+  group 'mysql'
+  mode '0777'
+  action :create
+end
+
+directory '/usr/local/mysql/sock' do
+  recursive true
+  owner 'mysql'
+  group 'mysql'
+  mode '0755'
+  action :create
+end
+
+## copying MySQL data
+# cd /var/lib/mysql
+# mv ibdata1 /MYSQL/cloud101/innodb/d1
+# mv ib_buffer_pool /MYSQL/cloud101/innodb/innodb_buffer_pool.info
+# mv ib_logfile* /MYSQL/cloud101/innodb-log
+# mv mysql performance_schema sys /MYSQL/cloud101/data/.
+# mv *.pem /MYSQL/cloud101/certs/.
+# 
+# \rm auto.cnf?
+# # ??? Where did /MYSQL/cloud101/data/mysql directory come from?
+
+
+#src_filepath = '/var/lib/mysql'
+#tar_filepath = '/MYSQL/cloud101'
+#bash 'Copying MySQL data' do
+  #cwd ::File.dirname('/var/lib/mysql')
+  #code <<-EOH
+    #mv ibdata1 #{tar_filepath}/innodb/d1
+    #mv ib_buffer_pool #{tar_filepath}/innodb/innodb_buffer_pool/info
+    #mv ib_logfile* #{tar_filepath}/innodb-log/
+    #mv mysql peformance_schema sys #{tar_filepath}/data/
+    #EOH
+  #not_if { ::File.exist?('/MYSQL/cloud101/data/innodb/d1') }
+#end
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -16,3 +16,140 @@ end
 describe port(80), :skip do
   it { should_not be_listening }
 end
+
+# End goal
+sql = mysql_session('root','XXXXX','localhost','3301')
+describe sql.query('select version() as version') do
+    its('stdout') { should match(/5.7.17/) }
+end
+
+#groupadd -g55000 mysql
+describe group('mysql') do
+  it { should exist }
+  its('gid') { should eq 55000 }
+end
+
+# useradd -u 55000 -g 55000 -c 'mysql-user' -m -d '/usr/local/mysql' -s '/bin/bash' -p '*' mysql
+# chmod 755 /usr/local/mysql
+describe user('mysql') do
+  it { should exist }
+  its('uid') { should eq 55000 }
+  its('gid') { should eq 55000 }
+  its('group') { should eq 'mysql' }
+  its('home') { should eq '/usr/local/mysql' }
+  its('shell') { should eq '/bin/bash' }
+end
+
+## download mysql repository
+describe file('/usr/local/src/mysql57-community-release-el7-11.noarch.rpm') do
+    it { should exist }
+end
+
+# sudo yum localinstall mysql57-community-release-el7-11.noarch.rpm
+describe yum do
+    its('mysql57-community/x86_64') { should exist }
+end
+
+describe yum.repo('mysql57-community/x86_64') do
+    it { should exist }
+    it { should be_enabled }
+    its('baseurl') { should include '5.7' }
+end
+
+
+# yum install mysql-community-server
+# yum install http://repo.mysql.com/yum/mysql-5.7-community/el/7/x86_64/mysql-community-server-5.7.17-1.el7.x86_64.rpm
+# yum install mysql-community-server-5.7.17-1.el7.x86_64 (OK)
+describe package('mysql-community-server') do
+    it { should be_installed }
+    its('version') { should eq '5.7.17-1.el7' }
+end
+
+# Create MySQL Directory
+# cd / ; mkdir -pvm 755 /MYSQL ; cd /MYSQL
+# mkdir -pvm 755 cloud101 ; cd cloud101;
+# mkdir -pvm 755 binlog data env innodb innodb-log log var work certs;
+# mkdir -pvm 777 /MYSQL/cloud101/work/tmp
+# cd ../ ; chown -R mysql:mysql cloud101 ;
+describe file("/MYSQL/cloud101") do
+   it { should exist }
+   it { should be_directory }
+   it { should be_owned_by 'mysql' }
+   its('mode') { should cmp '0755' }
+end
+
+%w[ binlog data env innodb innodb-log log var work certs ].each do | path | 
+  describe file("/MYSQL/cloud101/#{path}") do
+    it { should exist }
+    it { should be_directory }
+    it { should be_owned_by 'mysql' }
+    its('mode') { should cmp '0755' }
+  end
+end
+
+describe file("/MYSQL/cloud101/work/tmp") do
+   it { should exist }
+   it { should be_directory }
+   it { should be_owned_by 'mysql' }
+   its('mode') { should cmp '0777' }
+end
+
+describe file("/usr/local/mysql/sock") do
+   it { should exist }
+   it { should be_directory }
+   it { should be_owned_by 'mysql' }
+   its('mode') { should cmp '0755' }
+end
+
+## copying MySQL data
+# cd /var/lib/mysql ? Where is the directories and files?
+# mv ibdata1 /MYSQL/cloud101/innodb/d1
+# mv ib_buffer_pool /MYSQL/cloud101/innodb/innodb_buffer_pool.info
+# mv ib_logfile* /MYSQL/cloud101/innodb-log
+# mv mysql performance_schema sys /MYSQL/cloud101/data/.
+# mv *.pem /MYSQL/cloud101/certs/.
+#
+# \rm auto.cnf ? Why do I need to delete the file
+
+describe file("/var/lib/mysql") do
+   it { should exist }
+   it { should be_directory }
+end
+
+describe file("/var/lib/mysql/ibdata1") do
+   it { should exist }
+   it { should be_file }
+end
+
+describe file("/MYSQL/cloud101/innodb/d1") do
+   it { should exist }
+   it { should be_file }
+   it { should be_owned_by 'mysql' }
+end
+
+describe file("/MYSQL/cloud101/innodb/innodb_buffer_pool.info") do
+   it { should exist }
+   it { should be_file }
+   it { should be_owned_by 'mysql' }
+end
+
+describe file("/MYSQL/cloud101/innodb-log/ib_logfile*") do
+   it { should exist }
+   it { should be_file }
+   it { should be_owned_by 'mysql' }
+end
+
+%w[ mysql performance_schema sys ].each do | path | 
+  describe file("/MYSQL/cloud101/data/#{path}") do
+    it { should exist }
+    it { should be_directory }
+    it { should be_owned_by 'mysql' }
+    its('mode') { should cmp '0755' }
+  end
+end
+
+describe file("/MYSQL/cloud101/certs/*.pem") do
+   it { should exist }
+   it { should be_file }
+   it { should be_owned_by 'mysql' }
+end
